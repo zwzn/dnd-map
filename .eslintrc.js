6604c7f2150a520294dcb1f1790868bc117d6513
Adam Bibby <adam_bibby@hotmail.com>
2020-03-24 11:53:21 -0400

    basic website

---
+++ .eslintrc.js
@@ -0,0 +1,43 @@
+module.exports = {
+    root: true,
+    parser: '@typescript-eslint/parser',
+    plugins: [
+        '@typescript-eslint',
+        'react-hooks',
+    ],
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/eslint-recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:react/recommended',
+    ],
+    settings: {
+        "react": {
+            "pragma": "createElement",
+            "version": "detect",
+        },
+        "linkComponents": [
+            { "name": "Link", "linkAttribute": "to" }
+        ]
+    },
+    rules: {
+        '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^createElement$' }],
+        '@typescript-eslint/no-inferrable-types': 'off',
+        '@typescript-eslint/explicit-function-return-type': ['warn', {
+            allowExpressions: true,
+            allowTypedFunctionExpressions: true,
+            allowHigherOrderFunctions: true,
+        }],
+        'eqeqeq': ['warn', 'always'],
+        'jsx-quotes': ["warn", "prefer-single"],
+        'no-multiple-empty-lines': ["warn", { max: 1, maxEOF: 0, maxBOF: 0 }],
+        'react-hooks/exhaustive-deps': 'warn',
+        'react-hooks/rules-of-hooks': 'error',
+        'react/react-in-jsx-scope': 'off',
+        'react/prop-types': 'off',
+        'react/display-name': 'off',
+        'semi': ["warn", "never"],
+        "comma-dangle": ["warn", "always-multiline"],
+        "indent": ["warn", 4],
+    }
+};
